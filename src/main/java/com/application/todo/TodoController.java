e7739f6ed6ddfa461fd8c550307a2b0e4093acb5
alameen-2911 <itzalameen2004@gmail.com>
2026-04-11 11:36:59 +0530

    feat: verified CRUD endpoints with Postman

--- src/main/java/com/application/todo/TodoController.java
+++ src/main/java/com/application/todo/TodoController.java
@@ -1,12 +1,44 @@
 package com.application.todo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/todo") // Added a base path for better organization
 public class TodoController {
-    @GetMapping("/hello")
-    String sayHelloWorld(){
-        return "Hello world!";
+
+    // Get all
+    @GetMapping("/get")
+    public String getTodo() {
+        return "Todo";
+    }
+
+    // Path Variable
+    @GetMapping("/{id}")
+    public String getTodoById(@PathVariable long id) {
+        return "Todo with Id " + id;
+    }
+
+    // Request Param
+    @GetMapping("/search")
+    public String getTodoByIdParam(@RequestParam("todoId") long id) {
+        return "Todo with Id " + id;
+    }
+
+    // Post mapping
+    @PostMapping("/create")
+    public String createUser(@RequestBody String body) {
+        return body;
+    }
+
+    // Put mapping
+    @PutMapping("/{id}")
+    public String updateTodoById(@PathVariable long id) {
+        return "Update Todo with Id " + id;
+    }
+
+    // Delete mapping
+    @DeleteMapping("/{id}")
+    public String deleteTodoById(@PathVariable long id) {
+        return "Delete Todo with Id " + id;
     }
 }
