e095ebed8f03fe2dc6b692f31c5222914901f3aa
alameen-2911 <itzalameen2004@gmail.com>
2026-04-13 16:16:12 +0530

    feat: implement AuthController with Bcrypt password hashing and JWT login

---
+++ src/main/java/com/application/todo/service/UserService.java
@@ -0,0 +1,35 @@
+package com.application.todo.service;
+
+import com.application.todo.models.User;
+import com.application.todo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+    // Autowire
+    @Autowired
+    private UserRepository userRepository;
+
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User getUserById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User updateUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public void deleteUserById(Long id) {
+        userRepository.delete(getUserById(id));
+    }
+}
